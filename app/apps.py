7909c34e77ee698b31846a3701780dd3ee2e63a8
Sony Alamsyah Megadianaw <sonyalamsyahmegadiana92@gmail.com>
2020-10-21 09:37:09 +0700

    add error message

--- app/apps.py
+++ app/apps.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
-from flask import g, request, redirect
+from flask import g, request, redirect, jsonify
+from pymongo import errors
 
 import json
 
@@ -8,8 +9,6 @@ EXCLUDE = ["login", "static"]
 
 def refresh_session(rguid):
     pass
-
-
 
 
 def apps_blueprint_construct(dbs, session):
@@ -34,8 +33,53 @@ def apps_blueprint_construct(dbs, session):
                     is_logged = True
                     sess = json.loads(sess)
                     g.user = sess
-            
+
             if is_logged == False:
                 return redirect("/login", code=302)
+
+
+    @apps_bp.errorhandler(errors.ServerSelectionTimeoutError)
+    def mongodb_unactive(error):
+        return jsonify({
+            "success": False,
+            "message": str(error),
+            "data": []
+        }), 500
+    
+
+    @apps_bp.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "message": error.description.get("message", "Bad request."),
+            "data": []
+        }), 400
+
+
+    @apps_bp.errorhandler(401)
+    def unauthenticate_handler(error):
+        return jsonify({
+            "success": False,
+            "message": error.description.get("message", "you are unauthenticate."),
+            "data": []
+        }), 401
+    
+
+    @apps_bp.errorhandler(403)
+    def unauthorize_handler(error):
+        return jsonify({
+            "success": False,
+            "message": error.description.get("message", "Forbidden Access! You are unauthorize."),
+            "data": []
+        }), 403
+
+    
+    @apps_bp.errorhandler(404)
+    def not_found_handler(error):
+        return jsonify({
+            "success": False,
+            "message": error.description.get("message", "Not Found 404"),
+            "data": []
+        }), 404
 
     return apps_bp
